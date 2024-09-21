07bb2e72481c02deb6b040fa95b467da1a9f5b61
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2024-09-21 15:22:13 -0400

    updated db source

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -85,7 +85,6 @@ export const ChatInput = ({apiUrl, query, name, type}: ChatInputProps) => {
                    </FormItem>
                )} />
             </form>
-
         </Form>
     )
 }
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { Delete, Edit } from "lucide-react";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
-import { MembersModal } from "@/components/modals/members.-modal";
+import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
